81a0449dd06be5c2f84dd2368a6ace50ec2ba8ee
vladimirpodr <vladimirpodr@gmail.com>
2020-03-03 23:31:11 +0200

    first stage

--- terraform/vpcs.tf
+++ terraform/vpcs.tf
@@ -27,8 +27,8 @@ module "vpc-east" {
 }
 
 resource "aws_vpc_peering_connection" "pc" {
-  peer_vpc_id = module.vpc-west.vpc_id
-  vpc_id      = module.vpc-east.vpc_id
+# peer_vpc_id = module.vpc-west.vpc_id
+#  vpc_id      = module.vpc-east.vpc_id
   auto_accept = true
 
   accepter {
@@ -44,21 +44,21 @@ resource "aws_vpc_peering_connection" "pc" {
   }
 }
 
-resource "aws_route" "vpc-peering-route-east" {
-  count                     = 2
-  route_table_id            = module.vpc-east.privat_route_table_ids[0]
-  destination_cidr_block    = module.vpc-west.public_subnets_cidr_blocks[count.index]
-  vpc_peering_connection_id = aws_vpc_peering_connection.pc.id
-}
+#resource "aws_route" "vpc-peering-route-east" {
+#  count                     = 2
+#  route_table_id            = module.vpc-east.privat_route_table_ids[0]
+#  destination_cidr_block    = module.vpc-west.public_subnets_cidr_blocks[count.index]
+#  vpc_peering_connection_id = aws_vpc_peering_connection.pc.id
+#}
 
-resource "aws_route" "vpc-peering-route-west" {
-  count                     = 2
-  route_table_id            = module.vpc-west.public_route_table_ids[0]
-  destination_cidr_block    = module.vpc-east.privat_subnets_cidr_blocks[count.index]
-  vpc_peering_connection_id = aws_vpc_peering_connection.pc.id
-}
+#resource "aws_route" "vpc-peering-route-west" {
+#  count                     = 2
+#  route_table_id            = module.vpc-west.public_route_table_ids[0]
+#  destination_cidr_block    = module.vpc-east.privat_subnets_cidr_blocks[count.index]
+#  vpc_peering_connection_id = aws_vpc_peering_connection.pc.id
+#}
 
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id       = module.vpc-west.main_id
-  service_name = "com.amazonaws.s3-website-us-east-1.s3-static-test-page"
-}
+#resource "aws_vpc_endpoint" "s3" {
+#  vpc_id       = module.vpc-west.vpc_id
+#  service_name = "com.amazonaws.s3-website-us-east-1.s3-static-test-page"
+#}
